eaeca41c129769522bb626aca0a7455b781a57e0
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2020-04-22 14:56:52 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 30.0.0
    The following sniffs now pass and were enabled:
    * MediaWiki.Commenting.FunctionComment.MissingParamTag
    * MediaWiki.Commenting.FunctionComment.ParamNameNoMatch
    
    Change-Id: I8cc31fd6277c04861ae8312efa55534055cd5cc3

--- MultiMaps.body.php
+++ MultiMaps.body.php
@@ -17,7 +17,7 @@ class MultiMaps {
 	/**
 	 * Render map on wikipage using appropriate service class
 	 *
-	 * @param Parser $parser
+	 * @param Parser &$parser
 	 * @return string
 	 */
 	public static function renderParserFunction_showmap( Parser &$parser ) {
